b846b5071ea3c92fbd5f9dc9eec2c4e288c13d3e
YasinRezvani <yasinrezvani2025@gmail.com>
2021-07-30 23:21:36 +0430

    define a function main to get name of the file in path and after with os.rename rename a file

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 ﻿import os 
+
+def main():
+    i = 1
+    path = input("Enter The Path of Your files (example: C:/Users/yasin/Documents/test): ") + '/'
+    file_name = input("What is Name Your File To Change (example: Yasin): ")
+    format_file = input("What is your Format File (example: .pdf or .png): " )
+    for name in os.listdir(path):
+        new_name = file_name + str(i) + format_file
+        source = path + name
+        new_source = path + new_name
+        os.rename(source,new_source)
+        i += 1
+        print("--File Change Successfully--")
+if  __name__ == '__main__':
+    main()
+
+# Made By Yasin Rezvani     
